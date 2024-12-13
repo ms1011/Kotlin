69707920b53b4afa8effc27439fc52de2afb1de9
DESKTOP-L6SHIAA\소프트퍼즐 <ms1011@naver.com>
2024-12-13 12:11:42 +0900

    var, val 변수 사용 방법

--- src/Main.kt
+++ src/Main.kt
@@ -1,3 +1,16 @@
 fun main() {
     println("Hello World!");
+    variables();
+}
+
+fun variables() {
+    var name = "홍길동";       // 가변 String (text)
+    val birthYear = 1988;     // 불가변 Int (number)
+    println(name);
+    println(birthYear);
+
+    var name2: String = "홍길순";
+    val birthYear2: Int = 1992;
+    println(name2);
+    println(birthYear2);
 }
